7fb3adc847c71dd2a19872eb8c317402fc465eee
Daniel Blackburn <craklyn@gmail.com>
2017-06-14 16:19:08 -0700

    Fixing typo??

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from adtrackQuery.query import adtrackQuery
 
 
-print(adtrackQuery("SELECT * FROM at_call_log cl WHERE cl.parent_id='Ch4NI1f1OV86zQAb' LIMIT 10"))
+print(adtrackQuery("SELECT * FROM at_call_log cl WHERE cl.parent_id='Ch4Ni1f1OV86zQAb' LIMIT 10"))
